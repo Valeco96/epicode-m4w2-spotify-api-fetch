c31abff69904b0e126e544b2480e41f4cfcb8e0b
Valeco96 <correalev2@gmail.com>
2025-06-10 12:19:07 +0200

    Created search function and connected it to the search button in HTML

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
 console.log("Spotify premium");
 
-// function fetchEminemAlbums() {
-//   fetch("https://striveschool-api.herokuapp.com/search?q=eminem")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       const songs = data.data;
-//       const eminemSection = document.getElementById("eminemSection");
-//       const eminemBox = document.getElementById("eminem");
+const searchInput = document.getElementById("searchField");
 
-//       //elimina il d-none per mostrare la sezione
-//       eminemBox.classList.remove("d-none");
-
-//       //svuotalo del contenuto precedente
-//       eminemSection.innerHTML = "";
-
-//       songs.forEach((song) => {
-//         const col = document.createElement("div");
-//         col.className = "col mb-4";
-
-//         col.innerHTML = `<div class = "card h=100 shadow-sm">
-//         <img src = "${song.album.cover_medium}" class = "card-img-top" alt = "${song.title}">
-//         <div class = "card-body">
-//         <h5 class = "card-title">${song.title}</h5>
-//         <p class = "card-text">${song.artist.name}</p>
-//         <audio controls src = "${song.preview}"></audio>
-//         </div>
-//         </div>`;
-//       });
-//     });
-// }
+function search() {
+  //input deve prendere il valore che l'utente sta cercando
+  let elementoCercato = searchInput.value;
+  console.log(elementoCercato);
+}
 
 function fetchBrani(artista) {
   fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + artista)
